dde46de931cf8484f3fefd8cb1860651dde94828
Miguerubsk <miguerubsk@gmail.com>
2020-03-23 13:50:30 +0100

    clase escuadron creada

---
+++ POO_pr_6/Escuadron.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Escuadron.cpp
+ * Author: Miguerubsk
+ * 
+ * Created on 23 de marzo de 2020, 13:50
+ */
+
+#include "Escuadron.h"
+
+Escuadron::Escuadron() {
+}
+
+Escuadron::Escuadron(const Escuadron& orig) {
+}
+
+Escuadron::~Escuadron() {
+}
+
---
+++ POO_pr_6/Escuadron.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Escuadron.h
+ * Author: Miguerubsk
+ *
+ * Created on 23 de marzo de 2020, 13:50
+ */
+
+#ifndef ESCUADRON_H
+#define ESCUADRON_H
+
+class Escuadron {
+public:
+    Escuadron();
+    Escuadron(const Escuadron& orig);
+    virtual ~Escuadron();
+private:
+
+};
+
+#endif /* ESCUADRON_H */
+
